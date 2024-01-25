0fef96f85bedd83c06c317d8fbc045ba2a64f2b1
rdayupay <raqueldayupay@gmail.com>
2024-01-25 20:20:54 +0800

    Create new component to handle guess characters

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Guess from "../Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, i) => (
-        <p key={i} className="guess">
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
